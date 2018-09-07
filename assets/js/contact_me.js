7fbcca0dccc815551a18dde97d7c0fa245726892
SuelenGC <suelengcarvalho@gmail.com>
2018-09-06 23:21:22 -0300

    Minnor on contact

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -48,7 +48,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Desculpe, " + firstName + ", ocorreu algum problema. Por favor, tente novamente mais tarde!");
+                    $('#success > .alert-danger').append("<strong>Desculpe, ocorreu algum problema. Por favor, tente novamente mais tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
