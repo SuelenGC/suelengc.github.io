1ca5ecfcd03c77fb403cef749b9df2bf77bf7e15
SuelenGC <suelengcarvalho@gmail.com>
2019-03-12 23:28:55 -0300

    Fix and improve tag url

--- _plugins/_tag_gen.rb
+++ _plugins/_tag_gen.rb
@@ -19,6 +19,7 @@ module Jekyll
       if site.layouts.key? 'tag'
         dir = site.config['tag_dir'] || 'tag'
         site.tags.keys.each do |tag|
+          tag = tag.gsub(' ', '-').gsub('é', 'e').gsub('á', 'a').gsub('à', 'a').gsub('í', 'i').gsub('ú', 'u').gsub('ó', 'o')
           write_tag_index(site, File.join(dir, tag), tag)
         end
       end
